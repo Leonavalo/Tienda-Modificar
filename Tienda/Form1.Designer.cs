331e6e18d8058c0d3280d0c676af50256dbc3c6b
Leonavalo <77171356+Leonavalo@users.noreply.github.com>
2021-01-15 07:34:00 -0800

    Add files via upload

---
+++ Tienda/Form1.Designer.cs
@@ -0,0 +1,269 @@
+﻿namespace Tienda
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtcodigo = new System.Windows.Forms.TextBox();
+            this.txtlista = new System.Windows.Forms.TextBox();
+            this.btnagregar = new System.Windows.Forms.Button();
+            this.btnquitar = new System.Windows.Forms.Button();
+            this.btnpdf = new System.Windows.Forms.Button();
+            this.lblReloj = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.btnpagar = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.txt_box = new System.Windows.Forms.TextBox();
+            this.textBox_observer1 = new System.Windows.Forms.TextBox();
+            this.textBox_observer2 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Tienda.Properties.Resources.Tienda;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 30);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(139, 131);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(167, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(172, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Abarrotes El Milagro";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(168, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Código";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(157, 99);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Productos";
+            // 
+            // txtcodigo
+            // 
+            this.txtcodigo.Location = new System.Drawing.Point(215, 59);
+            this.txtcodigo.Name = "txtcodigo";
+            this.txtcodigo.Size = new System.Drawing.Size(136, 20);
+            this.txtcodigo.TabIndex = 4;
+            // 
+            // txtlista
+            // 
+            this.txtlista.Location = new System.Drawing.Point(215, 96);
+            this.txtlista.Multiline = true;
+            this.txtlista.Name = "txtlista";
+            this.txtlista.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtlista.Size = new System.Drawing.Size(288, 141);
+            this.txtlista.TabIndex = 5;
+            // 
+            // btnagregar
+            // 
+            this.btnagregar.Location = new System.Drawing.Point(359, 57);
+            this.btnagregar.Name = "btnagregar";
+            this.btnagregar.Size = new System.Drawing.Size(75, 23);
+            this.btnagregar.TabIndex = 6;
+            this.btnagregar.Text = "Agregar";
+            this.btnagregar.UseVisualStyleBackColor = true;
+            this.btnagregar.Click += new System.EventHandler(this.btnagregar_Click);
+            // 
+            // btnquitar
+            // 
+            this.btnquitar.Location = new System.Drawing.Point(509, 96);
+            this.btnquitar.Name = "btnquitar";
+            this.btnquitar.Size = new System.Drawing.Size(75, 23);
+            this.btnquitar.TabIndex = 7;
+            this.btnquitar.Text = "Quitar";
+            this.btnquitar.UseVisualStyleBackColor = true;
+            // 
+            // btnpdf
+            // 
+            this.btnpdf.Location = new System.Drawing.Point(509, 158);
+            this.btnpdf.Name = "btnpdf";
+            this.btnpdf.Size = new System.Drawing.Size(75, 23);
+            this.btnpdf.TabIndex = 8;
+            this.btnpdf.Text = "PDF";
+            this.btnpdf.UseVisualStyleBackColor = true;
+            // 
+            // lblReloj
+            // 
+            this.lblReloj.AutoSize = true;
+            this.lblReloj.Location = new System.Drawing.Point(12, 9);
+            this.lblReloj.Name = "lblReloj";
+            this.lblReloj.Size = new System.Drawing.Size(31, 13);
+            this.lblReloj.TabIndex = 9;
+            this.lblReloj.Text = "Reloj";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Location = new System.Drawing.Point(394, 240);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(40, 13);
+            this.lblTotal.TabIndex = 10;
+            this.lblTotal.Text = "Total =";
+            // 
+            // btnpagar
+            // 
+            this.btnpagar.Location = new System.Drawing.Point(509, 214);
+            this.btnpagar.Name = "btnpagar";
+            this.btnpagar.Size = new System.Drawing.Size(75, 23);
+            this.btnpagar.TabIndex = 11;
+            this.btnpagar.Text = "Pagar";
+            this.btnpagar.UseVisualStyleBackColor = true;
+            this.btnpagar.Click += new System.EventHandler(this.btnpagar_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(16, 178);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(55, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Productos";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Items.AddRange(new object[] {
+            "1-Coca Cola",
+            "2-Pepsi",
+            "3-Pan Bimbo",
+            "4-Leche"});
+            this.listBox1.Location = new System.Drawing.Point(15, 203);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(71, 56);
+            this.listBox1.TabIndex = 13;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // txt_box
+            // 
+            this.txt_box.Location = new System.Drawing.Point(51, 295);
+            this.txt_box.Name = "txt_box";
+            this.txt_box.Size = new System.Drawing.Size(100, 20);
+            this.txt_box.TabIndex = 14;
+            this.txt_box.Enter += new System.EventHandler(this.txt_box_Enter);
+            this.txt_box.Leave += new System.EventHandler(this.txt_box_Leave);
+            // 
+            // textBox_observer1
+            // 
+            this.textBox_observer1.Location = new System.Drawing.Point(51, 321);
+            this.textBox_observer1.Name = "textBox_observer1";
+            this.textBox_observer1.Size = new System.Drawing.Size(100, 20);
+            this.textBox_observer1.TabIndex = 15;
+            // 
+            // textBox_observer2
+            // 
+            this.textBox_observer2.Location = new System.Drawing.Point(51, 347);
+            this.textBox_observer2.Name = "textBox_observer2";
+            this.textBox_observer2.Size = new System.Drawing.Size(100, 20);
+            this.textBox_observer2.TabIndex = 16;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(677, 462);
+            this.Controls.Add(this.textBox_observer2);
+            this.Controls.Add(this.textBox_observer1);
+            this.Controls.Add(this.txt_box);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnpagar);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.lblReloj);
+            this.Controls.Add(this.btnpdf);
+            this.Controls.Add(this.btnquitar);
+            this.Controls.Add(this.btnagregar);
+            this.Controls.Add(this.txtlista);
+            this.Controls.Add(this.txtcodigo);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtcodigo;
+        private System.Windows.Forms.TextBox txtlista;
+        private System.Windows.Forms.Button btnagregar;
+        private System.Windows.Forms.Button btnquitar;
+        private System.Windows.Forms.Button btnpdf;
+        private System.Windows.Forms.Label lblReloj;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.Button btnpagar;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox txt_box;
+        private System.Windows.Forms.TextBox textBox_observer1;
+        private System.Windows.Forms.TextBox textBox_observer2;
+    }
+}
+
---
+++ Tienda/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Input;
+
+
+namespace Tienda
+{
+    public partial class Form1 : Form
+    {
+        //Datos en la base de datos
+        //Coca cola
+        int c1 = 1, c2 = 2, c3 = 3, c4 = 4;
+        string n1 = "Coca-cola", n2 = "pepsi", n3 = "Pan Bimbo", n4 = "Leche";
+        string d1 = "355 ml", d2 = "355 ml", d3 = "640 gramos 23 piezas", d4 = "1 Lt";
+
+        private void btnpagar_Click(object sender, EventArgs e)
+        {
+            //Añadir clase para desplegar nueva pantalla
+
+        }
+
+        int can1 = 10, can2 = 10, can3 = 14, can4 = 20;
+
+        private void txt_box_Enter(object sender, EventArgs e)
+        {
+            subj.Changed(txt_box.Text);
+        }
+
+        private void txt_box_Leave(object sender, EventArgs e)
+        {
+            subj.Changed(txt_box.Text);
+        }
+
+       
+
+        float p1 = 13, p2 = 12, p3 = 30, p4 = 16;
+        float T=0;
+        private void btnagregar_Click(object sender, EventArgs e)
+        {
+            int x;
+            
+            x = Convert.ToInt32(txtcodigo.Text);
+            if (x == 1)
+            {
+                txtlista.Text = txtlista.Text + "\r\n" + c1.ToString() + " " + n1 + " " + d1 +  " " + "$" + p1.ToString() ;
+                T = T + p1;
+                lblTotal.Text = "Total = $" + T.ToString();
+            }
+
+
+        }   
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        Subject subj = new Subject();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            Observer obs1 = new Observer(textBox_observer1);
+            Observer obs2 = new Observer(textBox_observer2);
+
+            subj.Subscribe(obs1);
+            subj.Subscribe(obs2);
+
+            subj.Changed(txt_box.Text);
+        }
+
+       // private void textBox_subject_LostFocus(object sender, EventArgs e)
+        //{
+         //   subj.changed(textBox_subject.Text);
+      //  }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            lblReloj.Text = DateTime.Now.ToLongTimeString();
+        }
+    }
+}
